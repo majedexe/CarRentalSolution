2e997fc631be575ae1df9c9a9b8d6893343ca3bc
Majed Alotaibi <Majedfeh@gmail.com>
2025-12-08 23:31:18 +0300

    rent model + Entity

---
+++ Car-Rental/Models/Rental.cs
@@ -0,0 +1,43 @@
+﻿using Car_Rental.Models.Enums;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car_Rental.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Car")]
+        public int CarId { get; set; }
+
+        public Car Car { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "User")]
+        public string UserId { get; set; } = string.Empty;
+
+        public IdentityUser User { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Start date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Total price")]
+        [DataType(DataType.Currency)]
+        public decimal TotalPrice { get; set; }
+
+        [Display(Name = "Status")]
+        public RentalStatus Status { get; set; } = RentalStatus.Active;
+
+        [Display(Name = "Created at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
